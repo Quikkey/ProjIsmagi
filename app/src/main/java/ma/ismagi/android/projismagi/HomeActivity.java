26c0c8fb86b1a08d2f30b9f7255dd725c334e9b9
Yassine zoubairi <yassinezoubairi1997@gmail.com>
2024-05-28 20:55:57 +0100

    Your descriptive message here

--- app/src/main/java/ma/ismagi/android/projismagi/HomeActivity.java
+++ app/src/main/java/ma/ismagi/android/projismagi/HomeActivity.java
@@ -46,9 +46,24 @@ public class HomeActivity extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(new Intent(HomeActivity.this , LabTestAcitivy.class));
             }
+
+        });
+
+        CardView orderDetails = findViewById(R.id.cardOrderDetails);
+        orderDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    startActivity(new Intent(HomeActivity.this , OrderDetailsActivity.class));
+            }
         });
 
 
-
+        CardView buyMedicine = findViewById(R.id.cardBuyMedicine);
+        buyMedicine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this , BuyMedicineActivity.class));
+            }
+        });
     }
 }
